60dd176bbadc2f541e17a38b352fd6284245563a
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-03-16 11:43:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
 //your code here
+let page = document.getElementById('page_number')
+let list = document.getElementById('issue_list')
+let nextbutton = document.getElementById('load_next')
+let prevbutton = document.getElementById('load_prev')
+
+
+let pageNumbers = 1;
+nextbutton.addEventListener("click",() => {
+  pageNumbers++;
+  if(pageNumbers>5){
+    pageNumbers = 5
+  }
+  updateUser(pageNumbers)
+})
+
+prevbutton.addEventListener("click",() => {
+  if(pageNumbers>1){
+    pageNumbers--;
+  }
+  updateUser(pageNumbers)
+})
+
+
+
+function updateUser(pageNumber){
+  fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`)
+    .then(response => response.json())
+    .then(data => {
+       page.innerText = `Page number ${pageNumber}`
+       list.innerHTML = ""
+
+        data.map(issue => {
+          let olLists = document.createElement('li')
+
+          
+            let stringRandom = randomNumber()
+            if(pageNumber === 1){
+              olLists.innerText = issue.title
+            }else{
+              olLists.innerText = issue.title + stringRandom
+            }
+            
+            list.appendChild(olLists)
+
+        })
+    })
+}
+
+updateUser(pageNumbers)
+
+function randomNumber(){
+  let string = ""
+  let characters = "sdjfasliudgfbrgnvkjsiugd"
+  
+  for(let i = 0;i<3;i++){
+    let number = Math.floor(Math.random()*24)
+    string += characters.charAt(number)
+  }
+  return string
+}
